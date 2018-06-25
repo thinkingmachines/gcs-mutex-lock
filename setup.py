e7d448bdf54cb80de12f720134e1dd3c875b1e40
iman <syvlabs@users.noreply.github.com>
2018-06-25 14:17:32 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='gcs-file-lock',
+      version='0.0.1',
+      description='File locks in Google Cloud Storage',
+      url='https://github.com/thinkingmachines/ml-tools',
+      author='Thinking Machines',
+      author_email='hello@thinkingmachin.es',
+      license='Apache License 2.0',
+      install_requires=[
+      ],
+      packages=find_packages(),
+      zip_safe=False)
