616c907e2d789adab97593ca3d54063bad58bf55
iman <syvlabs@users.noreply.github.com>
2018-06-26 08:38:43 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import sys
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 logging.info("Test start")
-acquired = gcs_lock.wait_for_lock_expo('gs://psc-demand/dataflow/temp_lock.txt', jitter=None)
+acquired = gcs_lock.wait_for_lock_expo('gs://psc-demand/dataflow/temp_lock.txt', max_time=60, jitter=None)
 print('Acquired? '+str(acquired))
 
 gcs_lock.unlock('gs://psc-demand/dataflow/temp_lock.txt')
