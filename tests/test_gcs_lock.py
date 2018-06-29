f4e5d0e79d649457f879a7130791388e2809c872
iman <iman@syvlabs.com>
2018-06-29 18:04:22 +0800

    trial

--- tests/test_gcs_lock.py
+++ tests/test_gcs_lock.py
@@ -4,4 +4,5 @@ from gcs_mutex_lock import gcs_lock
 # noinspection PyProtectedMember
 def test_lock_basic():
     # Make sure no error
-    gcs_lock.lock('gs://psc-demand/dataflow/hello.txt')
+    acquired = gcs_lock.lock('gs://psc-demand/dataflow/hello.txt')
+    assert acquired
